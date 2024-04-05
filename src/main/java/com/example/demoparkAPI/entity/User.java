39ec730e7fb110b9e43950fb14173cb2719c970c
MuriloSG <muriloprojetosti@gmail.com>
2024-04-05 13:39:39 -0300

    Add methods Entity users

--- src/main/java/com/example/demoparkAPI/entity/User.java
+++ src/main/java/com/example/demoparkAPI/entity/User.java
@@ -1,10 +1,17 @@
 package com.example.demoparkAPI.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity()
 @Table(name="users")
 public class User implements Serializable {
@@ -32,5 +39,25 @@ public class User implements Serializable {
 
   public enum Role{
     ROLE_ADMIN, ROLE_CLIENT
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(id, user.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            '}';
   }
 }
