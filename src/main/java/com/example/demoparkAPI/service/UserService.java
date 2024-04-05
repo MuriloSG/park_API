e8fbb802b6eedb2f6c980ca724fe3f5b586aabb3
MuriloSG <muriloprojetosti@gmail.com>
2024-04-05 14:27:50 -0300

    Create Service for create user in dataBase

--- src/main/java/com/example/demoparkAPI/service/UserService.java
+++ src/main/java/com/example/demoparkAPI/service/UserService.java
@@ -1,12 +1,18 @@
 package com.example.demoparkAPI.service;
 
+import com.example.demoparkAPI.entity.User;
 import com.example.demoparkAPI.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
   private final UserRepository userRepository;
+
+  @Transactional
+  public User save(User user) {
+    return userRepository.save(user);
+  }
 }
--- src/main/java/com/example/demoparkAPI/web/controller/UserController.java
+++ src/main/java/com/example/demoparkAPI/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.example.demoparkAPI.web.controller;
 
+import com.example.demoparkAPI.entity.User;
 import com.example.demoparkAPI.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/users")
 public class UserController {
   private  final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<User> create(@RequestBody User user){
+    User createUser = userService.save(user);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+  }
 }
